d4bcba7e991419dfc486eb8dccdbe33d857e0687
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-05-30 21:08:05 +0900

    ✨ Add constructors for PairTerm to support various parameter combinations

--- src/term/onsite.jl
+++ src/term/onsite.jl
@@ -2,3 +2,7 @@ struct OnsiteTerm{O<:AbstractOperator,T<:Number} <: AbstractTerm{T}
     op::O
     coeff::T
 end
+
+function OnsiteTerm(op::O) where {O<:AbstractOperator}
+    return OnsiteTerm{O,Float64}(op, 1.0)
+end
--- src/term/pair.jl
+++ src/term/pair.jl
@@ -4,3 +4,31 @@ struct PairTerm{O1<:AbstractOperator,O2<:AbstractOperator,T<:Number} <: Abstract
     coeff::T
     shell::Int
 end
+
+function PairTerm(op::O, coeff::T, shell::Int) where {O<:AbstractOperator,T<:Number}
+    return PairTerm{O,O,T}(op, op, coeff, shell)
+end
+
+function PairTerm(op1::O1, op2::O2, shell::Int) where {O1<:AbstractOperator,O2<:AbstractOperator}
+    return PairTerm{O1,O2,Float64}(op1, op2, 1.0, shell)
+end
+
+function PairTerm(op1::O1, op2::O2, coeff::T) where {O1<:AbstractOperator,O2<:AbstractOperator,T<:Number}
+    return PairTerm{O1,O2,T}(op1, op2, coeff, 1)
+end
+
+function PairTerm(op1::O1, op2::O2) where {O1<:AbstractOperator,O2<:AbstractOperator}
+    return PairTerm{O1,O2,Float64}(op1, op2, 1.0, 1)
+end
+
+function PairTerm(op::O, shell::Int) where {O<:AbstractOperator}
+    return PairTerm{O,O,Float64}(op, op, 1.0, shell)
+end
+
+function PairTerm(op::O, coeff::T) where {O<:AbstractOperator,T<:Number}
+    return PairTerm{O,O,T}(op, op, coeff, 1)
+end
+
+function PairTerm(op::O) where {O<:AbstractOperator}
+    return PairTerm{O,O,Float64}(op, op, 1.0, 1)
+end
