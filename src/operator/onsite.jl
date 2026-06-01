c8d188a561d6bd59097f62ffd8a53d7e1cd5d94d
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-06-02 02:49:42 +0900

    ♻️ Refactor display methods for OnsiteOperator and PairOperator to use nameof for improved clarity and consistency

--- src/operator/onsite.jl
+++ src/operator/onsite.jl
@@ -8,11 +8,14 @@ function OnsiteOperator(id::Int, pr::P) where {P<:AbstractOperatorPrimitive}
     return OnsiteOperator{P}(id, pr, 1.0)
 end
 
-Base.show(io::IO, op::OnsiteOperator{P}) where {P<:AbstractOperatorPrimitive} = print(io, "OnsiteOperator(id=$(op.id), $(op.pr), coeff=$(op.coeff))")
+function Base.show(io::IO, op::OnsiteOperator{P}) where {P<:AbstractOperatorPrimitive}
+    @printf io "OnsiteOperator(id=%d, %s, coeff=%f)" op.id nameof(typeof(op.pr)) op.coeff
+end
+
 function Base.show(io::IO, ::MIME"text/plain", op::OnsiteOperator{P}) where {P<:AbstractOperatorPrimitive}
     @printf io "[OnsiteOperator] \n"
     @printf io "id:          %d\n" op.id
-    @printf io "primitive:   %s\n" string(op.pr)
+    @printf io "primitive:   %s\n" nameof(typeof(op.pr))
     @printf io "coefficient: %f\n" op.coeff
 end
 
@@ -26,9 +29,12 @@ function UniformOnsiteOperator(pr::P) where {P<:AbstractOperatorPrimitive}
     return UniformOnsiteOperator{P}(pr, 1.0)
 end
 
-Base.show(io::IO, op::UniformOnsiteOperator{P}) where {P<:AbstractOperatorPrimitive} = print(io, "UniformOnsiteOperator($(op.pr), coeff=$(op.coeff))")
+function Base.show(io::IO, op::UniformOnsiteOperator{P}) where {P<:AbstractOperatorPrimitive}
+    @printf io "UniformOnsiteOperator(%s, coeff=%f)" nameof(typeof(op.pr)) op.coeff
+end
+
 function Base.show(io::IO, ::MIME"text/plain", op::UniformOnsiteOperator{P}) where {P<:AbstractOperatorPrimitive}
     @printf io "[UniformOnsiteOperator] \n"
-    @printf io "primitive:   %s\n" string(op.pr)
+    @printf io "primitive:   %s\n" nameof(typeof(op.pr))
     @printf io "coefficient: %f\n" op.coeff
 end
--- src/operator/pair.jl
+++ src/operator/pair.jl
@@ -32,11 +32,14 @@ function PairOperator(
     return PairOperator{P,P}(id1, id2, pr, pr, 1.0)
 end
 
-Base.show(io::IO, op::PairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive} = print(io, "PairOperator(id1=$(op.id1), id2=$(op.id2), $(op.pr1), $(op.pr2), coeff=$(op.coeff))")
+function Base.show(io::IO, op::PairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive}
+    @printf io "PairOperator(id1=%d, id2=%d, %s, %s, coeff=%f)" op.id1, op.id2, nameof(typeof(op.pr1)), nameof(typeof(op.pr2)), op.coeff
+end
+
 function Base.show(io::IO, ::MIME"text/plain", op::PairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive}
     @printf io "[PairOperator] \n"
     @printf io "id:          (%d, %d)\n" op.id1, op.id2
-    @printf io "primitive:   (%s, %s)\n" string(op.pr1), string(op.pr2)
+    @printf io "primitive:   (%s, %s)\n" nameof(typeof(op.pr1)), nameof(typeof(op.pr2))
     @printf io "coefficient: %f\n" op.coeff
 end
 
@@ -92,10 +95,12 @@ function UniformPairOperator(
     return UniformPairOperator{P,P}(pr, pr, coeff, 1)
 end
 
-Base.show(io::IO, op::UniformPairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive} = print(io, "UniformPairOperator($(op.pr1), $(op.pr2), coeff=$(op.coeff), shell=$(op.shell))")
+function Base.show(io::IO, op::UniformPairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive}
+    @printf io "UniformPairOperator(%s, %s, coeff=%f, shell=%d)" nameof(typeof(op.pr1)), nameof(typeof(op.pr2)), op.coeff, op.shell
+end
 function Base.show(io::IO, ::MIME"text/plain", op::UniformPairOperator{P1,P2}) where {P1<:AbstractOperatorPrimitive,P2<:AbstractOperatorPrimitive}
     @printf io "[UniformPairOperator]\n"
-    @printf io "primitive:   (%s, %s)\n" string(op.pr1), string(op.pr2)
+    @printf io "primitive:   (%s, %s)\n" nameof(typeof(op.pr1)), nameof(typeof(op.pr2))
     @printf io "coefficient: %f\n" op.coeff
     @printf io "shell:       %d\n" op.shell
 end
